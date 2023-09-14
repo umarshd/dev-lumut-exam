41db08978ac7c21a4a46fef20eecf2980ff912d8
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2023-09-14 13:48:43 +0700

    fixing nama model di post model

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class AccountModel extends Model
+class PostModel extends Model
 {
     protected $table = 'post';
     protected $primaryKey = 'idpost';
