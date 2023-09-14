a78183285c74d40a0f6306fd1331bd95ed84a7bf
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2023-09-14 14:26:56 +0700

    Menambahkan fungsi login

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -18,4 +18,58 @@ class Auth extends BaseController
         ];
         return view('auth/login', $data);
     }
+
+    public function prosesLogin()
+    {
+        $rules = $this->validate([
+            'username' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Username tidak boleh kosong'
+                ]
+            ],
+            'password' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Password tidak boleh kosong'
+                ]
+            ]
+        ]);
+
+        if (!$rules) {
+            session()->setFlashdata('errors', $this->validator->listErrors());
+            return redirect()->to('/auth/login')->withInput();
+        }
+
+        $username = $this->request->getVar('username');
+        $password = $this->request->getVar('password');
+
+        $cek = $this->AccountModel->where('username', $username)->first();
+
+        if (!$cek) {
+            session()->setFlashdata('errors', 'Username atau password salah');
+            return redirect()->to('/auth/login')->withInput();
+        }
+
+        $password_hash = password_verify($password, $cek['password']);
+
+        if (!$password_hash) {
+            session()->setFlashdata('errors', 'Username atau password salah');
+            return redirect()->to('/auth/login')->withInput();
+        }
+
+        $data = [
+            'username' => $cek['username'],
+            'role' => $cek['role'],
+            'logged_in' => TRUE
+        ];
+
+        session()->set($data);
+
+        if ($cek['role'] == 'admin') {
+            return redirect()->to('/admin');
+        } else {
+            return redirect()->to('/author');
+        }
+    }
 }
