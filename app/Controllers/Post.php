727435f43b46cb5cda5223ef29abd42c7bc924b8
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2023-09-14 14:09:33 +0700

    Pindah Controller post dari admin ke umum

---
+++ app/Controllers/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Post extends BaseController
+{
+    protected $PostModel;
+
+    public function __construct()
+    {
+        $this->PostModel = new \App\Models\PostModel();
+    }
+
+    public function index()
+    {
+        $data = [
+            'title' => 'Post',
+            'posts' => $this->PostModel->findAll()
+        ];
+        return view('admin/post/index', $data);
+    }
+
+
+}
