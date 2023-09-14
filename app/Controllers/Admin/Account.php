d6997058951e1641f32ffa0fcb66451d697cd608
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2023-09-14 14:04:16 +0700

    Menambahkan logic edit account di admin

--- app/Controllers/Admin/Account.php
+++ app/Controllers/Admin/Account.php
@@ -92,4 +92,110 @@ class Account extends BaseController
         ];
         return view('admin/account/edit', $data);
     }
+
+    public function prosesEdit()
+    {
+        $rules = $this->validate([
+            'username' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Username harus diisi.',
+                ]
+            ],
+            'name' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Nama harus diisi.'
+                ]
+            ],
+            'role' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Role harus diisi.'
+                ]
+            ]
+        ]);
+
+        if (!$rules) {
+            session()->setFlashdata('errors', $this->validator->listErrors());
+            return redirect()->back()->withInput();
+        }
+
+        $username = $this->request->getVar('username');
+        $username_lama = $this->request->getVar('username_lama');
+
+        $password = $this->request->getVar('password');
+        $password_konfirmasi = $this->request->getVar('password_konfirmasi');
+
+        if ($username == $username_lama) {
+            if ($password) {
+                if ($password != $password_konfirmasi) {
+                    session()->setFlashdata('errors', 'Password konfirmasi tidak sama.');
+                    return redirect()->back()->withInput();
+                }
+                $data = [
+                    'name' => $this->request->getVar('name'),
+                    'password' => password_hash($password, PASSWORD_DEFAULT),
+                    'role' => $this->request->getVar('role')
+                ];
+
+                $this->AccountModel->update($username, $data);
+                session()->setFlashdata('success', 'Berhasil mengubah data.');
+                return redirect()->to('/admin/account');
+
+
+            } else {
+                $data = [
+                    'name' => $this->request->getVar('name'),
+                    'role' => $this->request->getVar('role')
+                ];
+
+                $this->AccountModel->update($username, $data);
+                session()->setFlashdata('success', 'Berhasil mengubah data.');
+                return redirect()->to('/admin/account');
+            }
+        }
+
+        $rulesUsername = $this->validate([
+            'username' => [
+                'rules' => 'required|is_unique[account.username]',
+                'errors' => [
+                    'required' => 'Username harus diisi.',
+                    'is_unique' => 'Username sudah terdaftar.'
+                ]
+            ],
+        ]);
+
+        if (!$rulesUsername) {
+            session()->setFlashdata('errors', $this->validator->listErrors());
+            return redirect()->back()->withInput();
+        }
+
+        if ($password) {
+            if ($password != $password_konfirmasi) {
+                session()->setFlashdata('errors', 'Password konfirmasi tidak sama.');
+                return redirect()->back()->withInput();
+            }
+            $data = [
+                'username' => $this->request->getVar('username'),
+                'name' => $this->request->getVar('name'),
+                'password' => password_hash($password, PASSWORD_DEFAULT),
+                'role' => $this->request->getVar('role')
+            ];
+
+            $this->AccountModel->update($username, $data);
+            session()->setFlashdata('success', 'Berhasil mengubah data.');
+            return redirect()->to('/admin/account');
+        }
+
+        $data = [
+            'username' => $this->request->getVar('username'),
+            'name' => $this->request->getVar('name'),
+            'role' => $this->request->getVar('role')
+        ];
+
+        $this->AccountModel->update($username, $data);
+        session()->setFlashdata('success', 'Berhasil mengubah data.');
+        return redirect()->to('/admin/account');
+    }
 }
